0ad07f0a3ba4177cb7b8cb382f45facf501acb51
Anton Yemelyanov <anton.yemelyanov@gmail.com>
2021-04-08 13:45:51 -0400

    2.3.0 release

--- index.js
+++ index.js
@@ -208,15 +208,15 @@ export class KdxPortal extends FlowApp {
         let userOS = this.getOS();
         let selected = '';
         let contents = Object.entries({
-            'kdx-v2.2.0-windows-x64.exe' : 'installer',
+            'kdx-v2.3.0-windows-x64.exe' : 'installer',
 //            'kdx-dev-v1.2.0-windows-x64.exe' : 'installer',
 //            'kdx-v1.2.0-windows-x64.zip' : 'portable',
 //            'kdx-dev-v1.2.0-windows-x64.zip' : 'portable',
-            'kdx-v2.2.0-darwin-x64.dmg' : 'DMG',
+            'kdx-v2.3.0-darwin-x64.dmg' : 'DMG',
 //            'kdx-dev-v1.2.0-darwin-x64.dmg' : 'DMG',
             //'kdx-v1.2.0-darwin-x64.tar.gz' : 'portable',
             //'-kdx-1.0.3-darwin-x64.zip' : 'portable',
-            '-kdx-v2.2.0-linux-x64.zip' : 'portable'
+            '-kdx-v2.3.0-linux-x64.zip' : 'portable'
         }).map(([file, descr]) => {
             let disable = '';
             if(file.charAt(0) == '-') {
