31983618389a087ce8d124358c87f6d23e751d2d
Surinder Singh Matoo <surinder83singh@gmail.com>
2022-09-10 21:28:16 +0530

    2.11.1

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ export class KdxPortal extends FlowApp {
                             </div>
 
                             <div style="max-width:400px;">
-                                Current release: &nbsp; KDX 2.10.12 &nbsp; Kaspa 0.12.4 (master)<br/>&nbsp;<br/>
+                                Current release: &nbsp; KDX 2.11.1 &nbsp; Kaspa 0.12.6 (master)<br/>&nbsp;<br/>
                                 <div bright-red attn>After downloading KDX, you can get Kaspa from the Kaspa Faucet at <a href="https://faucet.kaspanet.io" target="_blank">https://faucet.kaspanet.io</a></div>
                                 
                             </div>
@@ -212,17 +212,17 @@ export class KdxPortal extends FlowApp {
         let userOS = this.getOS();
         let selected = '';
         let contents = Object.entries({
-            'kdx-v2.10.12-windows-x64.exe' : 'installer',
+            'kdx-v2.11.1-windows-x64.exe' : 'installer',
 //            'kdx-v2.10.0-windows-x64.exe' : 'installer',
 //            'kdx-dev-v1.2.0-windows-x64.exe' : 'installer',
 //            'kdx-v1.2.0-windows-x64.zip' : 'portable',
 //            'kdx-dev-v1.2.0-windows-x64.zip' : 'portable',
-            'kdx-v2.10.12-darwin-x64.dmg' : 'DMG',
+            'kdx-v2.11.1-darwin-x64.dmg' : 'DMG',
 //            'kdx-v2.5.2-darwin-x64.dmg' : 'DMG',
 //            'kdx-dev-v1.2.0-darwin-x64.dmg' : 'DMG',
             //'kdx-v1.2.0-darwin-x64.tar.gz' : 'portable',
             //'-kdx-1.0.3-darwin-x64.zip' : 'portable',
-            '-kdx-v2.10.12-linux-x64.zip' : 'portable'
+            '-kdx-v2.11.1-linux-x64.zip' : 'portable'
         }).map(([file, descr]) => {
             let disable = '';
             if(file.charAt(0) == '-') {
